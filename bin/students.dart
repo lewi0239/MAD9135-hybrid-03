4312329f3a130f7bfbab12ee95fd708f327eadf6
lewi0239 <lewi0239@algonquinlive.com>
2025-02-14 17:35:51 -0500

    updated student to meet new changes for assignment

--- bin/students.dart
+++ bin/students.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /* Class: MAD9135
  * Assignment: Hybrid-03
  * Student Name: Brodie Lewis
@@ -11,31 +13,25 @@ class Students {
 
   Students(this.people);
 
-  /// Sorts the List based on the given field (e.g., 'first' or 'email')
   void sort(String fieldName) {
     people.sort(
         (a, b) => a[fieldName].toString().compareTo(b[fieldName].toString()));
     output();
   }
 
-  /// Outputs each student's details
   void output() {
     for (var person in people) {
       print(person);
     }
   }
 
-  /// Adds a new student to the list and prints the updated list
   void plus(Map<String, dynamic> person) {
     people.add(person);
     output();
   }
 
-  /// Removes a specific field (key) from each student record
   void remove(String fieldName) {
-    for (var person in people) {
-      person.remove(fieldName);
-    }
+    people.removeWhere((person) => person.containsKey(fieldName));
     output();
   }
 }
